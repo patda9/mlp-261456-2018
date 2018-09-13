af9f3143571da88f1e4e1bcc28e36c511949f42a
Patdanai <patdanai.duangwaropas@gmail.com>
2018-09-13 23:15:27 +0700

    use adam optimizer in graient descent learning

--- src/mlp.py
+++ src/mlp.py
@@ -39,13 +39,25 @@ def forward(layers, input=[]):
         outputs.append(a)
     return outputs
 
-def back(d, layers, outputs):
+def back(i, d, layers, outputs, lr):
     e = d
+    means = []
+    variances = []
+    for o in reversed(outputs[1:]):
+        m = np.zeros(o.shape)
+        v = np.zeros(o.shape)
+        means.append(m)
+        variances.append(v)
+
+    j = 0
     for l in reversed(layers[1:]):
         output = outputs.pop()
         input = outputs[-1]
-        e = l.back(e, input, output)
-
+        result = l.back(e, input, output, i, means[j], variances[j], lr)
+        e = result[0]
+        gradient = result[1]
+        j += 1
+    
 def print_weights(layers):
     for l in layers[1:]:
         print(l.weight)
@@ -71,11 +83,17 @@ class Hidden(object):
         a = activation(z, self.act)
         return a
 
-    def back(self, e, input, output):
+    def back(self, e, input, output, t, m, v, lr, b1=.9, b2=.999, eps=1e-7):
         input = extend_bias(input)
         gradient = e * activation(output, self.act, d=True)
-        self.weight += input.T.dot(gradient) # l0T dot grad
-        return gradient.dot(self.weight[:-1].T) # error of previous layer
+
+        m = b1 * m + (1 - b1) * gradient # use adam optimization in gradient descent
+        v = b2 * v + (1 - b2) * (gradient ** 2)
+        m_hat = m/(1 - np.power(b1, t))
+        v_hat = v/(1 - np.power(b2, t))
+        inertia = m_hat/(np.sqrt(v_hat) + eps)
+        self.weight += (lr * np.amax(inertia)) * input.T.dot(gradient) # l0T dot grad
+        return [gradient.dot(self.weight[:-1].T), gradient] # error of previous layer
 
 class Output(object):
     def __init__(self, act, n_input, n):
@@ -88,12 +106,18 @@ class Output(object):
         a = activation(z, self.act)
         return a
 
-    def back(self, d, input, output):
+    def back(self, d, input, output, t, m, v, lr, b1=.9, b2=.999, eps=1e-7):
         e = d - output
         input = extend_bias(input)
+        
         gradient = e * activation(output, self.act, d=True)
-        self.weight += input.T.dot(gradient) # l0T dot grad
-        return gradient.dot(self.weight[:-1].T) # error of previous layer
+        m = b1 * m + (1 - b1) * gradient # use adam optimization in gradient descent
+        v = b2 * v + (1 - b2) * (gradient ** 2)
+        m_hat = m/(1 - np.power(b1, t))
+        v_hat = v/(1 - np.power(b2, t))
+        inertia = m_hat/(np.sqrt(v_hat) + eps)
+        self.weight += (lr * np.amax(inertia)) * input.T.dot(gradient) # l0T dot grad
+        return [gradient.dot(self.weight[:-1].T), gradient] # error of previous layer
 
 class MLP(object):
     def __init__(self, activations, form, input, output):
@@ -109,7 +133,7 @@ class MLP(object):
         layers.append(Output(activations[-1], form[-2], form[-1]))
         return layers
 
-def k_fold(activations, form, input, d, k):
+def k_fold(activations, form, input, d, k, learning_rate):
     # features[i] and label[i] must be at the same position after shuffling so concatenate them first
     data = np.concatenate((input, d), axis=1)
     np.random.shuffle(data)
@@ -140,7 +164,7 @@ def k_fold(activations, form, input, d, k):
         del(output_temp[i])
         training_set = input_temp
         d_train = output_temp
-        layers = train(activations, form, training_set[i], d_train[i])
+        layers = train(activations, form, training_set[i], d_train[i], learning_rate)
         fold_acc = test(layers, testing_set[i], d_test[i])
         sum_acc += fold_acc
         models.append(layers)
@@ -162,22 +186,24 @@ def test(layers, testing_set, d_test):
     avg_acc = np.mean(accuracy)
     return avg_acc
 
-def train(activations, form, training_set, d_train):
+def train(activations, form, training_set, d_train, learning_rate):
     model = MLP(activations, form, training_set, d_train)
     layers = model.init_network(activations, model.form)
     cost = np.inf
+
     i = 1
-    epochs = 2 ** 18
+    epochs = 2 ** 17
     while(cost > 1e-4 and i <= epochs):
         outputs = forward(layers)
         e = model.d - outputs[-1]
         cost = np.mean(np.abs(e))
         if(i % 32768 == 0):
             print(i, 'mse:' , cost)
-        back(model.d, layers, outputs)
+        back(i, model.d, layers, outputs, learning_rate)
         i += 1
     return layers
 
+learning_rate = .01
 activations = ['logistic', 'logistic', 'logistic',]
-form = [input.shape[1], 6, 4, 1]
-k_fold(activations, form, input, d, 10)
+form = [input.shape[1], 3, 3, 1]
+k_fold(activations, form, input, d, 10, learning_rate)
